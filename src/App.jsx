fc6b346bd12b69c52e32c0fd509c349a487ca277
Haider Ali <hyder6291@gmail.com>
2026-03-29 19:45:16 +0500

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full min-h-screen flex justify-center items-center '>
+      <p className='text-4xl font-mono'>React + Tailwind + Shadcn Template {"(Javascript Version)"} </p>
+      </div>
+  )
+}
+
+export default App
