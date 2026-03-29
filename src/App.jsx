c53cc409e9f6112f5c081f091c9117bc84b288cc
Haider Ali <hyder6291@gmail.com>
2026-03-29 19:46:37 +0500

    Simple Template for Quick Setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='w-full min-h-screen flex justify-center items-center '>
-      <p className='text-4xl font-mono'>React + Tailwind + Shadcn Template {"(Javascript Version)"} </p>
+    <div className='w-full min-h-screen flex justify-center items-center bg-foreground '>
+      <p className='text-4xl font-mono text-background'>React + Tailwind + Shadcn Template {"(Javascript Version)"} </p>
       </div>
   )
 }
